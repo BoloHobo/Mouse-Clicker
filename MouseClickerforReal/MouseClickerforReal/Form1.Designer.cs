bf0d423d4811418c7887411d36bedc6015e815ad
BoloHobo <76228547+BoloHobo@users.noreply.github.com>
2020-12-18 14:41:07 -0600

    Add files via upload

---
+++ MouseClickerforReal/MouseClickerforReal/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace MouseClickerforReal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(247, 36);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(417, 217);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(247, 290);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(417, 216);
+            this.btn_stop.TabIndex = 1;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(815, 302);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 622);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ MouseClickerforReal/MouseClickerforReal/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace MouseClickerforReal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        [DllImport("user32")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtrainfo);
+        private const int mouseeventf_leftdown = 0x02;
+        private const int mouseeventf_leftup = 0x04;
+        
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+        }
+        int keeptime = 0;
+        int clickTime = 0;
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            
+                timer1.Start();
+               // timer3.Start(); 
+           
+
+        }
+        bool intro = false;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            keeptime++;
+            
+            
+            if (keeptime % 600 == 0)
+            {
+                timer3.Start();
+                intro = true;
+            }
+            if (intro == true)
+            {
+                clickTime++;
+
+            }
+
+            label1.Text = keeptime.ToString();
+           
+        }
+        
+        public void PerformClick(int x, int y)
+        {
+            
+            
+            Application.DoEvents();
+            mouse_event(mouseeventf_leftdown, x, y, 0, 0);
+            mouse_event(mouseeventf_leftup, x, y, 0, 0);
+
+
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            keeptime = 0;
+            clickTime = 0;
+           
+        }
+        
+        private void timer2_Tick(object sender, EventArgs e)
+        {  
+
+           
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+        
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            
+            PerformClick(MousePosition.X, MousePosition.Y);
+            if(clickTime %100 == 0)
+            {
+                timer3.Stop();
+                keeptime = 0;
+                clickTime = 0;
+            }
+
+        }
+
+        private bool OnKeyPress(Keys escape)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+    }
+}
